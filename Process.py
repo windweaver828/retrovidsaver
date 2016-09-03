9933914446c741cc8da73ea5e54885ade84931f5
Keith Brandenburg <windweaver828@gmail.com>
2016-09-03 16:11:47 -0500

    Add started scripts

---
+++ Process.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+"Keith's Custom Process Functionality"
+
+import sys, os
+
+def chgProcessName(processName):
+    if sys.platform == 'linux2':
+        import ctypes
+        libc = ctypes.cdll.LoadLibrary('libc.so.6')
+        libc.prctl(15, processName, 0, 0, 0)
+        return True
+
+    elif sys.platform == 'win32':
+        raise NotImplementedError("Windows is not supported")
+    else:
+        raise NotImplementedError("Operating System not detected/supported")
+
+def isRunning(processName):
+    if sys.platform == 'linux2':
+        if not translate(processName): return False
+        else: return True
+    elif sys.platform == 'win32':
+        raise NotImplementedError("Windows is not supported")
+    else:
+        raise NotImplementedError("Operating System not detected/supported")
+
+def translate(processName):
+    if sys.platform == 'linux2':
+        cmd = "ps -A | grep -m 1 -w {pName}".format(pName=processName)
+        try:
+            ret = os.popen(cmd).readlines()[0].split()
+            pid = ret[0]
+            pname = ret[-1]
+        except IndexError:
+            return False
+        if pname == processName: return str(pid)
+        else: return pname
+    elif sys.platform == 'win32':
+        raise NotImplementedError("Windows is not supported")
+    else:
+        raise NotImplementedError("Operating System not detected/supported")
+
+def getCurPid():
+    return str(os.getpid())
+
+
---
+++ start_timer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+
+import os
+import sys
+import Process
+
+if Process.isRunning("xautolock"):
+    print("xautolock is already running")
+    sys.exit(0)
+
+cmd = "xautolock -time 600 -locker "
+cmd += os.path.expanduser("~/Video-Screensaver/screensaver.py") 
+
+os.popen(cmd)
+print("xautolock started")
